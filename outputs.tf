358a229502b5face988ce8ab514f8dadce9874b9
cezarmathe <me@cezarmathe.com>
2021-05-03 08:25:18 +0300

    Improve outputs documentation

--- outputs.tf
+++ outputs.tf
@@ -3,7 +3,7 @@
 output "config_volume_name" {
   description = <<-DESCRIPTION
   Name of the config volume.
-  If `create_config_volume` is set to `false` this output will hold an empty string.
+  If 'create_config_volume' is set to 'false' this output will hold an empty string.
   DESCRIPTION
   value       = var.create_config_volume ? docker_volume.config[0].name : ""
 }
@@ -11,7 +11,7 @@ output "config_volume_name" {
 output "watch_volume_name" {
   description = <<-DESCRIPTION
   Name of the watch volume.
-  If `create_watch_volume` is set to `false` this output will hold an empty string.
+  If 'create_watch_volume' is set to 'false' this output will hold an empty string.
   DESCRIPTION
   value       = var.create_watch_volume ? docker_volume.watch[0].name : ""
 }
@@ -19,7 +19,7 @@ output "watch_volume_name" {
 output "downloads_volume_name" {
   description = <<-DESCRIPTION
   Name of the downloads volume.
-  If `create_downloads_volume` is set to `false` this output will hold an empty string.
+  If 'create_downloads_volume' is set to 'false' this output will hold an empty string.
   DESCRIPTION
   value       = var.create_downloads_volume ? docker_volume.downloads[0].name : ""
 }
@@ -30,6 +30,9 @@ output "this_name" {
 }
 
 output "this_network_data" {
-  description = "Network configuration of the container."
+  description = <<-DESCRIPTION
+  Network configuration of the container. This exports the 'network_data' attribute of the docker
+  container, check the docker provider (kreuzwerker/docker) for more info.
+  DESCRIPTION
   value       = docker_container.this.network_data
 }
